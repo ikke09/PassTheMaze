e393b8003d0147cfa2c9a299c77dad0f5c5af6f2
Christian Bechtluft <christian.bechtluft@games-broduction.com>
2016-07-03 15:49:14 +0200

    Umstellung auf CreateJs!
    - phaser.io komponenten gelöscht und code rausgenommen
    + globale Variablen definiert
    + init() Methode implementiert
    + erste Linie gezeichnet für Test
    + style Datei hinzugefügt

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+//Globale Variablen deklarieren
+var stage;
+var gfx;
+var grid;
+
+
 var eWallSide = {
     TOP : {value: 0, name: "Top", direction: "north"},
     RIGHT : {value: 1, name: "Right", direction: "east"},
@@ -15,89 +21,34 @@ function Cell(posX, posY, size){
     this.Size = size;
     this.Walls = [new Wall(eWallSide.TOP), new Wall(eWallSide.RIGHT),new Wall(eWallSide.BOTTOM), new Wall(eWallSide.LEFT)];
     this.Visited = false;
-    
-    this.getLinestoDraw = function(){
-        
-        var topLine, rightLine, bottomLine, leftLine;
-        
-        for(var w = 0; w<this.Walls.length;w++){
-            
-            if(this.Walls[w] == undefined)
-                continue;
-        
-            if(this.Walls[w].Side == eWallSide.TOP)
-            {
-                topLine = new Phaser.Line(this.Walls[w].X,this.Walls[w].Y,this.Walls[w].X + this.Walls[w].Size, this.Walls[w].Y)
-            }else if(this.Walls[w].Side == eWallSide.RIGHT)
-            {
-                rightLine = new Phaser.Line(this.Walls[w].X + this.Walls[w].Size,this.Walls[w].Y,this.Walls[w].X, this.Walls[w].Y + this.Walls[w].Size);
-            }else if(this.Walls[w].Side == eWallSide.BOTTOM)
-            {
-                bottomLine = new Phaser.Line(this.Walls[w].X + this.Walls[w].Size,this.Walls[w].Y + this.Walls[w].Size, this.Walls[w].X, this.Walls[w].Y + this.Walls[w].Size);
-            }else if(this.Walls[w].Side == eWallSide.LEFT)
-            {
-                leftLine = new Phaser.Line(this.Walls[w].X,this.Walls[w].Y + this.Walls[w].Size, this.Walls[w].X, this.Walls[w].Y);
-            }
-        }
-        
-        return [topLine,rightLine,bottomLine,leftLine];
-    }
 }
 
-$(document).ready(function(){
+function init(){
     
     //Size of the html page at the start
     var screenHeight = $(window).height();
     var screenWidth = $(window).width();
 
-    //init Phaser GameObject
-    var game = new Phaser.Game(800,600, Phaser.CANVAS, '', {preload: preload, create: create, render: render, update: update});
+    $("#myCanvas").attr({width: ""+screenWidth, height: ""+screenHeight});
     
     //size of cells
     var cellSize = 10;
     var cellAmountWidth = Math.floor(screenWidth / cellSize);
     var cellAmountHeigth = Math.floor(screenHeight / cellSize);
     
-    //grid with cells in it
-    var grid;
+    stage = new createjs.Stage("myCanvas");
+    gfx = new createjs.Graphics();
+}
+
+$(document).ready(function(){
     
-    //graphics objects to render 
-    var graphics = new Phaser.Graphics(game);
-    
-    var bitmapData = new Phaser.BitmapData(game,"cell");
-    
-    function preload(){
-        console.log("IsFullscreeen: "+game.scale.isFullScreen);
-    }
-    
-    function create(){
-        
-        grid = new Array(cellAmountWidth);
-        for(var g = 0; g < grid.length; g++)
-        {
-            grid[g] = new Array(cellAmountHeigth);        
-        }
-        
-        console.log("create");
-        for(var i=0; i<grid.length;i++){
-            for (var k=0;k<grid[0].length;k++){
-                grid[i][k] = new Cell(i * cellSize, k*cellSize, cellSize);
-            }
-        }
-    }
-    
-     function update(){
-        console.log("update");
-    }
-    
-    function render(){
-        console.log("render");
-        for(var i=0; i<grid.length;i++){
-            for (var k=0;k<grid[0].length;k++){
-                var lines = grid[i][k].getLinestoDraw();
-                for(var line = 0; line < lines.length; line++)
-                    game.debug.geom(lines[line]);
-            }
-        }
-    }
+    init();
+    gfx.setStrokeStyle(3);
+    gfx.beginStroke("#fff");
+    gfx.moveTo(100,100);
+    gfx.lineTo(200,200);
+    gfx.endStroke();
+    var shape = new createjs.Shape(gfx);
+    stage.addChild(shape);
+    stage.update();
 });
