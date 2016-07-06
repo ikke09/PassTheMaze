e9761536a96f143db18198d4be64afa4052d7c53
Christian Bechtluft <christian.bechtluft@games-broduction.com>
2016-07-06 19:25:49 +0200

    + canvas etwas zentriert + komplette initalisierung abgeschlossen + Events registriert + Render und RemoveWall für die Cell implementiert
    --> Problem mit Rendering

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ var stage;
 var gfx;
 var grid;
 
-
 var eWallSide = {
     TOP : {value: 0, name: "Top", direction: "north"},
     RIGHT : {value: 1, name: "Right", direction: "east"},
@@ -23,32 +22,127 @@ function Cell(posX, posY, size){
     this.Visited = false;
 }
 
-function init(){
+//draws the Cell to the Screen
+Cell.prototype.Render = function(cellColor, wallColor){
     
-    //Size of the html page at the start
-    var screenHeight = $(window).height();
-    var screenWidth = $(window).width();
-
-    $("#myCanvas").attr({width: ""+screenWidth, height: ""+screenHeight});
+    console.log("Render Cell");
     
-    //size of cells
-    var cellSize = 10;
-    var cellAmountWidth = Math.floor(screenWidth / cellSize);
-    var cellAmountHeigth = Math.floor(screenHeight / cellSize);
+    if(gfx == undefined)
+        gfx = new createjs.Graphics();
     
-    stage = new createjs.Stage("myCanvas");
-    gfx = new createjs.Graphics();
-}
-
-$(document).ready(function(){
+    if(cellColor == undefined || cellColor === "")
+        cellColor = "#000";
     
-    init();
-    gfx.setStrokeStyle(3);
-    gfx.beginStroke("#fff");
-    gfx.moveTo(100,100);
-    gfx.lineTo(200,200);
+    if(wallColor == undefined || wallColor === "")
+        wallColor = "#fff";
+    
+    gfx.clear();
+    gfx.beginFill(cellColor).drawRect(this.X,this.Y,this.Size,this.Size);
+    gfx.endFill();
+    gfx.setStrokeStyle(1);
+    gfx.beginStroke(wallColor);
+    for(var i=0;i<this.Walls.length;i++)
+    {
+        if(this.Walls[i] == undefined)
+            continue;
+        
+        switch(this.Walls[i].Side){
+            case eWallSide.TOP:
+                gfx.moveTo(this.X,this.Y);
+                gfx.lineTo(this.X + this.Size, this.Y);
+                break;
+            case eWallSide.RIGHT:
+                gfx.moveTo(this.X + this.Size,this.Y);
+                gfx.lineTo(this.X + this.Size, this.Y + this.Size);
+                break;
+            case eWallSide.BOTTOM:
+                gfx.moveTo(this.X + this.Size, this.Y + this.Size);
+                gfx.lineTo(this.X, this.Y + this.Size);
+                break;
+            case eWallSide.LEFT:
+                gfx.moveTo(this.X, this.Y + this.Size);
+                gfx.lineTo(this.X,this.Y);
+                break;
+        }
+    }
     gfx.endStroke();
     var shape = new createjs.Shape(gfx);
     stage.addChild(shape);
+}
+
+//Removes the selected Wall from the Cell
+//returns true if a wall was removed
+Cell.prototype.RemoveWall = function(wallSide){
+    
+    if(wallSide == undefined)
+        return false;
+    
+    this.Walls.forEach(function(wall)
+    {
+        if(wall.Side == wallSide)
+        {
+            this.Walls.splice(this.Walls.indexOf(wall),1);
+            return true;
+        }
+    });
+    return false;
+}
+
+function init(){
+    
+    //Set size of the canvas to half of the webpage
+    var canvasWidth = Math.floor($(window).width() * 0.8);
+    var canvasHeight = Math.floor($(window).height() * 0.8);
+
+    $("#myCanvas").attr({width: ""+canvasWidth, height: ""+canvasHeight});
+    
+    //size of cells
+    cellsize = 10;
+    var cellAmountWidth = Math.floor(canvasWidth / cellsize);
+    var cellAmountHeigth = Math.floor(canvasHeight / cellsize);
+    
+    //create 2D grid Array
+    grid = new Array(cellAmountWidth);
+    for(var x=0;x<grid.length;x++){
+        grid[x] = new Array(cellAmountHeigth);
+    }
+    
+    //fill the grid with cells
+    for(var x=0;x<grid.length;x++){
+        for(var y=0;y< grid[0].length;y++){
+            grid[x][y] = new Cell(x * cellsize, y * cellsize, cellsize);
+        }
+    }
+    
+    //easeljs Stage initalisieren
+    stage = new createjs.Stage("myCanvas");
+    
+    //Events registrieren
+    createjs.Ticker.addEventListener("tick", handleTick);
+    createjs.Ticker.setFPS
+    stage.addEventListener("click", handleClick);
+    stage.addEventListener("mouseMove", handleMouseMove);
+}
+
+function handleTick(event){
+    //fill the grid with cells
+    for(var x=0;x<grid.length;x++){
+        for(var y=0;y< grid[0].length;y++){
+            grid[x][y].Render("#0000ff","00ff00");
+        }
+    }
     stage.update();
+}
+
+function handleClick(event){
+    
+}
+
+function handleMouseMove(event){
+    
+}
+
+$(document).ready(function(){
+    init();
 });
+
